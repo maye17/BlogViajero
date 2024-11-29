69f3df1e94f21ed477887b0ca14a02a1b8753be7
maye17 <mayerlin.dossantos@gmail.com>
2024-11-29 17:12:52 -0300

    agregando pagina de viajes por paises

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//Declarando los constantes
+const btnArgentina = document.getElementById('btnPeru');
+
+// Llamando al botón ver perú
+btnArgentina.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location.href = "http://127.0.0.1:5501/fdsw-github/pages/Peru.html#";
+});
