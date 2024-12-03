8a0a6263dae53dc1eb4af43df9a1d2f33de0cdb7
maye17 <mayerlin.dossantos@gmail.com>
2024-12-03 17:19:44 -0300

    agregando cards en la pagina de argentina

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 
-//Declarando los constantes
-const btnArgentina = document.getElementById('btnPeru');
 
-// Llamando al botón ver perú
-btnArgentina.addEventListener("click", function (e) {
+
+// Declatando Elementos
+const btnArgentina = document.getElementById('btnArgentina')
+const btnPeru = document.getElementById('btnPeru');
+
+//Llamando al botón
+btnPeru.addEventListener("click", function (e) {
     e.preventDefault();
     window.location.href = "http://127.0.0.1:5501/fdsw-github/pages/Peru.html#";
+});
+
+btnArgentina.addEventListener('click',function(e) {
+    e.preventDefault();
+    window.location.href='http://127.0.0.1:5501/fdsw-github/pages/Argentina.html#'
 });
